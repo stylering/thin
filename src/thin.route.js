5fc6e1ac5368589862cc0e00081805635e2893a1
stylering <stylering@126.com>
2014-08-12 17:50:56 +0800

    路由控制

--- src/thin.route.js
+++ src/thin.route.js
@@ -3,24 +3,68 @@
 	var doc = document;
 	var thin = win.thin || (win.thin = {});
 
-	thin.route = {
+	// backbone regExp
+	var optionalParam = /\((.*?)\)/g;
+	var namedParam    = /(\(\?)?:\w+/g;
+	var splatParam    = /\*\w+/g;
+	var escapeRegExp  = /[\-{}\[\]+?.,\\\^$|#\s]/g;
+
+	var Router = {
 		root: '/',
-		checkPath: function(path) {
+		routes: undefined,
+		fragment: undefined,
+		checkUrl: function(path) {
 			return path.replace(/^\//, '').replace(/\/$/, '').replace(/(.*)/, '\/$1\/');
 		},
-		routes: [],
 		add: function(o) {
 			
 		},
 		remove: function(o) {
 			
 		},
-		navigator: '',
+		navigator: function(){
+			
+		},
 		config: function(options) {
-			// 判断hash路径
-			if (options && options.root) {
-				this.root = checkPath(options.root);
+			options || options = {};
+			if (options.routes) {
+				this.routes = options.routes;
 			}
-		}
+			this._bindHandles();
+		},
+		route: function (route, callback) {
+			
+		},
+		start: function() {
+
+		},
+		_bindRoutes: function(){
+			var routes,
+				route;
+
+			routes = this.routes;
+			if (!routes) return;
+			if (typeof routes === 'function') {
+				routes = this.routes = routes();
+			}
+			for (route in routes) {
+				this.route(route, routes[route]);
+			}
+		},
+		_getFragment function() {
+
+		},
+		// 转换正则表达式
+		_routeToRegExp: function (route) {
+			route = route.replace(escapeRegExp, '\\$&')
+	             .replace(optionalParam, '(?:$1)?')
+	             .replace(namedParam, function(match, optional) {
+	               return optional ? match : '([^/?]+)';
+	             })
+	             .replace(splatParam, '([^?]*?)');
+			return new RegExp('^' + route + '(?:\\?([\\s\\S]*))?$');
+		},
 	}
+
+	thin.router = Router;
 }());
