193fe5a93b408e7c5b768838a8c76a1910775aa5
stylering <stylering@126.com>
2014-10-13 15:02:59 +0800

    update require module

--- src/thin.require.js
+++ src/thin.require.js
@@ -5,7 +5,14 @@
 	var thin = win.thin || (win.thin = {});
 
 	var basePath;
+	var head = doc.head || doc.getElementsByTagName('head')[0];
+
+	/*需要用到的正则表达式
+	 */
 	
+	// url生成序列号作为命名空间
+	var rmakeid = /(#.+|\W)/;
+
 	var Module = function() {
 		this.id = ''
 		this.deps = '',
@@ -14,7 +21,9 @@
 
 	Module.status = {
 		LOADING: 0,
-		LOADED: 1
+		LOADED: 1,
+		EXECUTING: 2,
+		EXECUTED: 3 
 	}
 
 	win.require = thin.require = function(list, factory) {
@@ -25,8 +34,39 @@
 
 	}
 
+	// 加载js和css资源文件
+	function requestSource(src) {
+		var node;
+
+		if (/.js$/.test(src)) {	// 加载js
+			var onloadSupport = 'onload' in node;
+			node = doc.createElement('script');
+
+			function onload() {
+				if (onloadSupport || /loaded|complete/i.test(node.readyState)) {
+					console.log('ddddd');
+				}
+			}
+			onloadSupport ? node.onload = onload : node.onreadystatechange = onload;
+			node.onerror = function() {
+				console.log('error');
+			}
+			node.src = src;
+			head.appendChild(node);
+		} else if (/.css$/.test(src)) {		// 加载css
+			var id = src.replace(rmakeid, '');
+			if (!doc.getElementById(id)) {
+				node = doc.createElement('link');
+				node.rel = 'stylesheet';
+				node.href = src;
+				node.id = id;
+				head.appendChild(node);
+			}
+		}
+	}
+
 	// 获取加载器的路径地址
-	function getBasePath() {
+	function getCurrentPath() {
 		var scripts, script,
 			len, 
 			i = 0;
